558f0c9435fcd363511c7367190a7e971a2ee256
chenxue <chenxue4076@163.com>
2019-11-15 18:31:05 +0800

    demo

--- demo.go
+++ demo.go
@@ -10,15 +10,15 @@ func main()  {
 	text := "<xml><ToUserName><![CDATA[oia2Tj我是中文jewbmiOUlr6X-1crbLOvLw]]></ToUserName><FromUserName><![CDATA[gh_7f083739789a]]></FromUserName><CreateTime>1407743423</CreateTime><MsgType><![CDATA[video]]></MsgType><Video><MediaId><![CDATA[eYJ1MbwPRJtOvIEabaxHs7TX2D-HV71s79GUxqdUkjm6Gs2Ed1KF3ulAOA9H1xG0]]></MediaId><Title><![CDATA[testCallBackReplyVideo]]></Title><Description><![CDATA[testCallBackReplyVideo]]></Description></Video></xml>"
 	timeStamp := "1409304348"
 	nonce := "xxxxxx"
-	wxBizMsgCrypt := wxMsgCrypt.Default("pamtest","abcdefghijklmnopqrstuvwxyz0123456789ABCDEFG","wxb11529c136998cb6")
+	wxBizMsgCrypt := msg_encrypt.Default("pamtest","abcdefghijklmnopqrstuvwxyz0123456789ABCDEFG","wxb11529c136998cb6")
 	encryptMsg, errorCode := wxBizMsgCrypt.EncryptMsg(text, timeStamp, nonce)
 	if errorCode != 0 {
-		fmt.Println("加密失败："+ wxMsgCrypt.WXBizMsgErrorMsg(errorCode))
+		fmt.Println("加密失败："+ msg_encrypt.WXBizMsgErrorMsg(errorCode))
 	}
 	fmt.Println("加密后："+encryptMsg)
 
 	//解析xml
-	msgEncryptFormat := wxMsgCrypt.MsgEncryptFormat{}
+	msgEncryptFormat := msg_encrypt.MsgEncryptFormat{}
 	err := xml.Unmarshal([]byte(encryptMsg), &msgEncryptFormat)
 	if err != nil {
 		fmt.Println("解析XML失败："+ err.Error())
@@ -30,7 +30,7 @@ func main()  {
 
 	wxBizMsgDecrypt, errorCode := wxBizMsgCrypt.DecryptMsg(msgEncryptFormat.MsgSignature, timeStamp, nonce, msgFormatXML)
 	if errorCode != 0 {
-		fmt.Println("解密失败："+ wxMsgCrypt.WXBizMsgErrorMsg(errorCode))
+		fmt.Println("解密失败："+ msg_encrypt.WXBizMsgErrorMsg(errorCode))
 	}
 	fmt.Println("解密后：",string(wxBizMsgDecrypt))
 
